f1f2556a7d7b0ce8723c50b091840c820cdc1342
muhammedkaraarslan <37312960+muhammedkaraarslan@users.noreply.github.com>
2026-05-04 18:49:16 -0400

    Unroll fully

--- dune/build-and-run.sh
+++ dune/build-and-run.sh
@@ -2,15 +2,26 @@
 
 set -e
 
-export CELER_LOG=debug
-export CELER_LOG_LOCAL=debug
+export CELER_LOG=info
+export CELER_LOG_LOCAL=info
 export CUDA_VISIBLE_DEVICES=6
 
+BUILD=/scratch/s3j/build/celeritas-release-orange
+
+# Rebuild executable with updated cmake/git metadata
 set -x
-/scratch/s3j/build/celeritas-release-orange/bin/celer-optical \
-  run.json
+(cd $BUILD \
+ && cmake -UCeleritas_CGV_CACHE . \
+ && ninja celer-optical)
+
+# Check that the GPU is not in use
+nvidia-smi pmon -c 1
+
+# Run and reformat output
+$BUILD/bin/celer-optical run.json
 jq . out.json > out.formatted.json && mv out.formatted.json out.json
 
+# Summarize
 jq '{
   total: .result.time.total,
   version: .system.build.version,
